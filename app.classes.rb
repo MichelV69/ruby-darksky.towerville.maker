1783445b91d5a86fc5193f4aa6e925a60e36db99
Michel R Vaillancourt <michel@wolfstar.ca>
2021-12-02 21:52:14 -0400

    Properly passing table-generation mock-up test.  GTG on building out the table.

--- app.classes.rb
+++ app.classes.rb
@@ -21,7 +21,20 @@ class Towerville2056
 
   # ---
   def getPrimaryIndustry(ptr)
-    {rollIndex: ptr.to_s, summaryDesc: "wrong", broadDesc: "still_wrong" }
+
+    primaryIndustryTable = []
+    primaryIndustryTable[1] = {summaryDesc: "Primary Sector", broadDesc: "Processing & Refining of externally extracted or harvested goods.  Ore, Minerals, Oils, Produce, Forest Products and the like are examples."}
+    primaryIndustryTable[2] = {summaryDesc: "Secondary Sector", broadDesc: "Processing, and construction using Primary Sector goods"}
+    primaryIndustryTable[3] = {summaryDesc: "wrong3", broadDesc: "still_wrong"}
+    primaryIndustryTable[4] = {summaryDesc: "wrong4", broadDesc: "still_wrong"}
+    primaryIndustryTable[5] = {summaryDesc: "wrong5", broadDesc: "still_wrong"}
+    primaryIndustryTable[6] = {summaryDesc: "wrong6", broadDesc: "still_wrong"}
+
+    tableRow = primaryIndustryTable[ptr]
+    f1 = tableRow[:summaryDesc]
+    f2 = tableRow[:broadDesc]
+    {rollIndex: ptr.to_s, summaryDesc: f1, broadDesc: f2}
+
   end
 
 end
