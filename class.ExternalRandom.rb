dd8b1669706824840f9df95519ac11d013a3491b
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2022-01-08 09:35:01 -0400

    Added ENV conditional output for debugging

--- class.ExternalRandom.rb
+++ class.ExternalRandom.rb
@@ -15,13 +15,16 @@ class	ExternalRandom
 	@rand_max=10000
 
 	def self.get(max)
-
+    putc ">" if (!ENV["RUBY_ENV"].nil? && ENV["RUBY_ENV"].contains?("dev"))
 		rg = RandomOrgHttpApi::Generator.new
+
+    putc ">" if (!ENV["RUBY_ENV"].nil? && ENV["RUBY_ENV"].contains?("dev"))
 		ten_thousand = rg.generate_integers(num: 1, max: @rand_max)
 
+    putc "<" if (!ENV["RUBY_ENV"].nil? && ENV["RUBY_ENV"].contains?("dev"))
     result = ((ten_thousand.first.to_f / @rand_max.to_f ) * max).to_i
 
-		#debug_output("#{max}|#{ten_thousand}|#{result}")
+    puts "!" if (!ENV["RUBY_ENV"].nil? && ENV["RUBY_ENV"].contains?("dev"))
 		return result
 	end
 end # class ExternalRandom
