fbd82321d10d2396a60d143a367e9766adb1c599
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2022-01-08 08:44:13 -0400

    developing Feature requirements

--- features/step_definitions/05.BuildingPillars-InlineDice.rb
+++ features/step_definitions/05.BuildingPillars-InlineDice.rb
@@ -4,4 +4,24 @@ require_relative('../../class.Towerville2056.rb')
 require_relative('../../class.ExternalRandom.rb')
 
 # ---
+
+Given('the Description for Pillars includes a random-rolled height') do
+  @example_string = "Pillars are [roll:2d4+2] in number and [roll:1d4+1d6+1] stories tall, and leave 50% void space of the foot print."
+end
+
+When('a formatted string like {string}') do |simple_dice_string|
+  @simple_dice_string = simple_dice_string
+end
+
+Then('the dice string should be parsed and replaced with a number') do
+  parsed_simple_dice_string = DiceString.parse(@simple_dice_string)
+  parsed_example_string     = DiceString.parse(@example_string)
+
+  expect(parsed_simple_dice_string).not_to  eq(@simple_dice_string)
+  expect(parsed_example_string).not_to      eq(@example_string)
+
+  expect(parsed_simple_dice_string).not_to  contain("[roll:")
+  expect(parsed_example_string).not_to      contain("[roll:")
+
+end
 # --- end of file ---
