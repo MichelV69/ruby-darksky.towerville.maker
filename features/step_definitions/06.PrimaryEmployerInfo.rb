b542ffbeae17f8e4815238b40c8751d51da55ac7
Michel R Vaillancourt <michel@wolfstar.ca>
2022-01-10 13:54:09 -0400

    Adding new Feature

---
+++ features/step_definitions/06.PrimaryEmployerInfo.rb
@@ -0,0 +1,7 @@
+require "rspec/expectations"
+require_relative('../../lib.wolfstar_studios.rb')
+require_relative('../../class.Towerville2056.rb')
+require_relative('../../class.ExternalRandom.rb')
+# ---
+
+# --- end of file ---
