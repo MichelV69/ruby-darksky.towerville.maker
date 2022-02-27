aad94c26408a7557d714eb91a280e528739056de
Michel R Vaillancourt <michel@michelrvaillancourt.com>
2022-02-27 09:06:14 -0400

    step definition skeletons for features to be implemented

---
+++ features/step_definitions/08.otherDetails.rb
@@ -0,0 +1,25 @@
+# 08.otherDetails.rb
+require "rspec/expectations"
+require_relative('../../lib.wolfstar_studios.rb')
+require_relative('../../class.Towerville2056.rb')
+require_relative('../../class.ExternalRandom.rb')
+
+# Montreal 7700 retail stores for 1,906,865 people
+
+Given('a {string} Test-Build Towerville') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I check for the number of shops') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('then I should get {int}') do |int|
+# Then('then I should get {float}') do |float|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I check for the number of social spaces') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+# ---
